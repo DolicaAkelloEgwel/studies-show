2c05dd64f15d873181b1871217e9b9cd903786da
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-06-06 15:23:15 +0100

    just some more line length tinkering

--- create_story.py
+++ create_story.py
@@ -16,22 +16,24 @@ def _create_story(article_title: str) -> ChatResponse:
             {
                 "role": "system",
                 "content": (
-                    "You are a news story generator. Given an article title, you will"
-                    " create an extremely silly made up but realistic-sounding 600+"
-                    " word news story that continues the theme of the article title."
-                    " Throw in lots of intimidating, clever words so that people think"
-                    ' "Well this MUST be true and not fake at all because the person'
-                    ' who wrote this is obviously so smart." If appropriate, throw in'
-                    " weird statistics or mention universities at which research was"
-                    " carried out or so on, or give the credentials of the experts who"
-                    " were consulted. Make it sound very verbose and wordy. Please make"
-                    " it UK centric."
+                    "You are a news story generator. Given an article title,"
+                    " you will create an extremely silly made up but"
+                    " realistic-sounding 600+ word news story that continues"
+                    " the theme of the article title. Throw in lots of"
+                    " intimidating, clever words so that people think 'Well"
+                    " this MUST be true and not fake at all because the person"
+                    " who wrote this is obviously so smart.' If appropriate,"
+                    " throw in weird statistics or mention universities at"
+                    " which research was carried out or so on, or give the"
+                    " credentials of the experts who were consulted. Make it"
+                    " sound very verbose and wordy. Please make it UK centric."
                 ),
             },
             {
                 "role": "user",
                 "content": (
-                    f"Please create an article with the given title: {article_title}"
+                    "Please create an article with the given title:"
+                    f" {article_title}"
                 ),
             },
         ],
@@ -40,7 +42,8 @@ def _create_story(article_title: str) -> ChatResponse:
 
 
 response: ChatResponse = _create_story(
-    "Quiet schoolteacher leads double-life as head of apocalypse squirrel cult."
+    "Quiet schoolteacher leads double-life as head of apocalyptic squirrel"
+    " cult!"
 )
 
 newsarticle = NewsArticle.model_validate_json(response.message.content)
