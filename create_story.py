24fe259245c85ec23894b0b76110cda4b411ff81
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-06-06 15:19:55 +0100

    so that's how it's done...

--- create_story.py
+++ create_story.py
@@ -15,11 +15,24 @@ def _create_story(article_title: str) -> ChatResponse:
         messages=[
             {
                 "role": "system",
-                "content": 'You are a news story generator. Given an article title, you will create an extremely silly made up but realistic-sounding 600+ word news story that continues the theme of the article title. Throw in lots of intimidating, clever words so that people think "Well this MUST be true and not fake at all because the person who wrote this is obviously so smart." If appropriate, throw in weird statistics or mention universities at which research was carried out or so on, or give the credentials of the experts who were consulted. Make it sound very verbose and wordy. Please make it UK centric.',
+                "content": (
+                    "You are a news story generator. Given an article title, you will"
+                    " create an extremely silly made up but realistic-sounding 600+"
+                    " word news story that continues the theme of the article title."
+                    " Throw in lots of intimidating, clever words so that people think"
+                    ' "Well this MUST be true and not fake at all because the person'
+                    ' who wrote this is obviously so smart." If appropriate, throw in'
+                    " weird statistics or mention universities at which research was"
+                    " carried out or so on, or give the credentials of the experts who"
+                    " were consulted. Make it sound very verbose and wordy. Please make"
+                    " it UK centric."
+                ),
             },
             {
                 "role": "user",
-                "content": f"Please create an article with the given title: {article_title}",
+                "content": (
+                    f"Please create an article with the given title: {article_title}"
+                ),
             },
         ],
         format=NewsArticle.model_json_schema(),
