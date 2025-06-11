6aae9d3b816dac3cc4e0da411f1e4008484fc4dd
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-06-11 18:24:24 +0100

    constants

--- constants.py
+++ constants.py
@@ -2,3 +2,5 @@ APP_TITLE = "ЯECOLLECTOЯ v 8.3.4"
 COMMANDS = ("search", "t-and-c", "help", "whats-new", "thanks")
 BLOCK_CHARACTER = "█"
 FONT_PATH = "/home/dolica/code/studies-show/assets/bedstead-20.bdf"
+LINE_Y_DISTANCE = 20
+TOP_LINE_Y = 40
