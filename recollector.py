c76cc24d256528bc9abc003c042b95ada529e15d
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-06-11 18:24:03 +0100

    some starting screen stuff

--- recollector.py
+++ recollector.py
@@ -1,11 +1,20 @@
+import math
+
 import pyxel
 
 
 class App:
-    def __init__(self, app_title: str, font_path: str):
-        pyxel.init(800, 600, title=app_title)
+    def __init__(self, app_title: str, font_path: str, presenter):
+        pyxel.init(1280, 900, title=app_title)
         self.bedstead = pyxel.Font(font_path)
+        self.presenter = presenter
+        with open("./logo", "r") as f:
+            logo = f.readlines()
+        self._text = logo + ["", "v8.3.4"]
         pyxel.run(self.update, self.draw)
+
+    def _set_text(self, display_text: list[str]):
+        self._text = display_text
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
@@ -13,5 +22,21 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "ЯECOLLECTOЯ v 8.3.4", 11, self.bedstead)
-        pyxel.text(55, 81, "[rec@cth] ENTER PASSWORD:", 11, self.bedstead)
+        i = 0
+        for line in self._text:
+            pyxel.text(
+                55,
+                41
+                + (
+                    (i * 20)
+                    + math.sin(pyxel.frame_count * 0.125 * 0.25) * 125
+                    + 100
+                ),
+                line,
+                11,
+                self.bedstead,
+            )
+            i += 1
+        if pyxel.frame_count % 30 < 25:
+            pyxel.text(55, 800, "[PRESS ANY KEY TO START]", 11, self.bedstead)
+        pyxel.text(55, 840, "© RANDOM COMPANY SOFTWARE", 11, self.bedstead)
