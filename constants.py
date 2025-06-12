91120efc074714c302c8ea8b3b715c4a854eb952
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2025-06-12 15:25:57 +0100

    windows stuff

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
+import os
+
 APP_TITLE = "ЯECOLLECTOЯ v 8.3.4"
 COMMANDS = ("search", "t-and-c", "help", "whats-new", "thanks")
 BLOCK_CHARACTER = "█"
-FONT_PATH = "/home/dolica/code/studies-show/assets/bedstead-20.bdf"
+FONT_PATH = os.path.join("assets", "bedstead-20.bdf")
 LINE_Y_DISTANCE = 20
 TOP_LINE_Y = 40
 
