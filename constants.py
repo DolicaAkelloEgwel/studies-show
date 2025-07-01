0b00ffc89dc77edaa6a2f2ea526586eba31f703c
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-07-01 11:59:48 +0100

    extra tests

--- constants.py
+++ constants.py
@@ -12,6 +12,7 @@ TOP_LINE_Y = 40
 RED_COL = 8
 
 APP_WIDTH = 1024
+HALF_APP_WIDTH = APP_WIDTH // 2
 APP_HEIGHT = 768
 
 START_TEXT = "[PRESS ANY KEY TO START]"
@@ -30,8 +31,8 @@ def text_centre_x(text: str) -> int:
     num_chars = len(text)
     if num_chars == 0:
         raise ValueError("String has length 0")
-    string_width_in_pixels = _width_of_string_in_pixels(text)
-    return APP_WIDTH // 2 - string_width_in_pixels // 2 - 2
+    string_width_in_pixels = _width_of_string_in_pixels(num_chars)
+    return HALF_APP_WIDTH - string_width_in_pixels // 2 - 2
 
 
 def get_vline_colour(show_vline: bool) -> int:
--- test_constants.py
+++ test_constants.py
@@ -16,6 +16,15 @@ class TestHelperFunctions(unittest.TestCase):
     def test_two_chars_then_pixel_width_is_22(self):
         self.assertEqual(constants._width_of_string_in_pixels(2), 22)
 
+    def test_given_empty_string_then_exception_thrown(self):
+        self.assertRaises(ValueError, lambda: constants.text_centre_x(""))
+
+    def test_given_single_char_string_then_centre_is_505(self):
+        self.assertEqual(constants.text_centre_x("A"), 505)
+
+    def test_given_two_char_string_then_centre_is_499(self):
+        self.assertEqual(constants.text_centre_x("AA"), 499)
+
 
 if __name__ == "__main__":
     unittest.main()
