0310ca6e22fee1dd7904d33adf24f7d7e559aa04
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-07-01 17:28:10 +0100

    terms and conditions bs

--- constants.py
+++ constants.py
@@ -6,7 +6,6 @@ COMMANDS = ("search", "t-and-c", "help", "whats-new", "thanks")
 BLOCK_CHARACTER = "█"
 ASSETS_PATH = "assets"
 FONT_PATH = os.path.join(ASSETS_PATH, "bedstead-20.bdf")
-LOGO_PATH = os.path.join(ASSETS_PATH, "logo")
 LINE_Y_DISTANCE = 20
 TOP_LINE_Y = 40
 RED_COL = 8
@@ -21,9 +20,14 @@ START_TEXT_Y = APP_HEIGHT - 120
 COPYRIGHT_TEXT = "(C) 2025 GRAVE MATTER"
 COPYRIGHT_TEXT_Y = APP_HEIGHT - 60
 
-with open(LOGO_PATH, "r") as f:
+with open(os.path.join(ASSETS_PATH, "logo"), "r") as f:
     LOGO = f.readlines()
 LOGO = LOGO + ["", VERSION]
+
+TERMS_AND_CONDITIONS_TITLE = "TERMS AND CONDITIONS"
+
+with open(os.path.join(ASSETS_PATH, "terms-and-conditions"), "r") as f:
+    TERMS_AND_CONDITIONS_TEXT = f.readlines()
 
 
 def _width_of_string_in_pixels(num_chars: int) -> int:
