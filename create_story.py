83ad7593c874bc02accd3d683d4c5f0b4e03f312
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-08-20 12:55:59 +0100

    found an image creating one that I like

--- create_story.py
+++ create_story.py
@@ -8,7 +8,7 @@ prompt_models = [
     "impactframes/llama3_ifai_sd_prompt_mkr_q4km:latest",
     "brxce/stable-diffusion-prompt-generator:latest",
 ]
-IMAGE_PROMPT_MODEL = "trollek/qwen2-diffusion-prompter"
+IMAGE_PROMPT_MODEL = "impactframes/llama3_ifai_sd_prompt_mkr_q4km:latest"
 ARTICLE_GENERATION_MODEL = "deepseek-r1:7b"
 
 
@@ -57,12 +57,6 @@ def _stable_diffusion_prompt(image_description: str, prompt_model: str) -> str:
     return chat(
         model=prompt_model,
         messages=[
-            # {
-            #     "role": "system",
-            #     "content": (
-            #         "Given an image description, you will create a text-to-image prompt that can generate that image. Please ensure the image is photorealistic and suitable for a newspaper. Do not say anything related to digital art."
-            #     ),
-            # },
             {
                 "role": "user",
                 "content": (
@@ -74,7 +68,7 @@ def _stable_diffusion_prompt(image_description: str, prompt_model: str) -> str:
 
 
 response: ChatResponse = _create_story(
-    "Researchers crack the Stonehenge code!"
+    "The pope steps down and says he's an atheist"
 )
 
 news_article = NewsArticle.model_validate_json(response.message.content)
