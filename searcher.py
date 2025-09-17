cae96fc4dd96adc84803073873ff59f365bb7d75
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-09-17 15:25:08 +0100

    I hope this works

--- searcher.py
+++ searcher.py
@@ -2,12 +2,14 @@ from create_image import create_image
 from create_story import create_story
 from printer_wrapper import print_document
 from write_document import write_document
+import time
 
 
 class Searcher:
     def __init__(self):
         self._search_in_progress = False
         self._begun_printing = False
+        self._finished_search = False
 
     def search(
         self,
@@ -21,13 +23,20 @@ class Searcher:
         if create_image:
             img = create_image(image_prompt)
             img.save("output.jpg")
-        write_document(article)
+        # write_document(article)
         self._begun_printing, self._search_in_progress = (
             self._search_in_progress,
             self._begun_printing,
         )
         if print:
             print_document()
+        time.sleep(10)
+        self._begun_printing, self._finished_search = (
+            self._finished_search,
+            self._begun_printing,
+        )
+        time.sleep(10)
+        self._finished_search = False
 
     @property
     def search_in_progress(self):
@@ -36,3 +45,15 @@ class Searcher:
     @property
     def begun_printing(self):
         return self._begun_printing
+
+    @property
+    def finished_search(self):
+        return self._finished_search
+
+    @property
+    def busy(self):
+        return (
+            self._search_in_progress
+            or self._begun_printing
+            or self._finished_search
+        )
