959a1f81f4d07739a7d060b37657ebb9a7109e7b
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-09-18 12:49:23 +0100

    using manager to report status

--- searcher.py
+++ searcher.py
@@ -7,11 +7,11 @@ from write_document import write_document
 
 
 class Searcher:
-    def __init__(self):
+    def __init__(self, manager):
         """An object for generating articles."""
-        self._search_in_progress = False
-        self._begun_printing = False
-        self._finished_search = False
+        self._search_in_progress = manager.Value("b", False)
+        self._begun_printing = manager.Value("b", False)
+        self._finished_search = manager.Value("b", False)
 
     def search(
         self,
@@ -28,42 +28,42 @@ class Searcher:
             create_image (bool, optional): Whether or not to create an image. Defaults to True.
             print (bool, optional): Whether or not to print the document. Defaults to True.
         """
-        self._search_in_progress = True
+        self._search_in_progress.value = True
         article, image_prompt = create_story(summary, year)
         if generate_image:
             img = create_image(image_prompt)
             img.save("output.jpg")
         write_document(article)
-        self._begun_printing, self._search_in_progress = (
-            self._search_in_progress,
-            self._begun_printing,
+        self._begun_printing.value, self._search_in_progress.value = (
+            self._search_in_progress.value,
+            self._begun_printing.value,
         )
         if print:
             print_document()
         time.sleep(10)
-        self._begun_printing, self._finished_search = (
-            self._finished_search,
-            self._begun_printing,
+        self._begun_printing.value, self._finished_search.value = (
+            self._finished_search.value,
+            self._begun_printing.value,
         )
         time.sleep(10)
-        self._finished_search = False
+        self._finished_search.value = False
 
     @property
     def search_in_progress(self):
-        return self._search_in_progress
+        return self._search_in_progress.value
 
     @property
     def begun_printing(self):
-        return self._begun_printing
+        return self._begun_printing.value
 
     @property
     def finished_search(self):
-        return self._finished_search
+        return self._finished_search.value
 
     @property
     def busy(self):
         return (
-            self._search_in_progress
-            or self._begun_printing
-            or self._finished_search
+            self.search_in_progress
+            or self.begun_printing
+            or self.finished_search
         )
